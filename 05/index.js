c526d9c8ca319468447e81c5595d6cf8afb5346f
Ailken <ailkencf@gmail.com>
2023-08-06 14:40:09 -0300

    exercicios de métodos de String exercicio 05 (esconder numero do cartão)

--- 05/index.js
+++ 05/index.js
@@ -1,1 +1,12 @@
+function formatarNumeroCartao(numeroCartao) {
+    const primeirosQuatroDigitos = numeroCartao.slice (0,4);
+    const ultimosQuatroDigitos = numeroCartao.slice (-4);
+    const digitosOcultos = '*'.repeat(numeroCartao.length - 8);
+
+    return primeirosQuatroDigitos + digitosOcultos + ultimosQuatroDigitos;
+}
+
 const numeroCartao = '1111222233334444';
+const numeroCartaoFormatado = formatarNumeroCartao(numeroCartao);
+
+console.log(numeroCartaoFormatado);
