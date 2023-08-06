40c0ae6ed1b920044d6c2c83222b1b6e030617ae
Ailken <ailkencf@gmail.com>
2023-08-06 15:16:56 -0300

    exercicios de métodos de String exercicio 08 (remover pontuação)

--- 08/index.js
+++ 08/index.js
@@ -1,1 +1,9 @@
+function removerPonto(cpfOuCnpj) {
+    const cpfOuCnpjSemPonto = cpfOuCnpj.split('.').join('').split('-').join('').split('/').join('');
+    console.log(cpfOuCnpjSemPonto); 
+}
+
+const cnpj = "021.525.0001-89"
 const cpf = "011.022.033-44";
+removerPonto(cpf);
+removerPonto(cnpj);
