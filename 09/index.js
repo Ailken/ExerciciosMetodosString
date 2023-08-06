7fbea99d1ed0ab9a3ec83c0f7b4a55519d2cc72a
Ailken <ailkencf@gmail.com>
2023-08-06 15:22:43 -0300

    exercicios de métodos de String exercicio 09 (Gerador de Nickname)

--- 09/index.js
+++ 09/index.js
@@ -1,1 +1,8 @@
-const nome = 'Guido Cerqueira';
+function gerarNick (nome) {
+    const nickname = '@' + nome.split(' ').map(part => part.toLowerCase()).join('').substring(0, 13);
+
+    return nickname;
+}
+
+const nome = 'Ailken Fidelis';
+console.log(gerarNick(nome));
