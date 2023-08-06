7fba2c2ff05bcde1d3e156a753ef42edff9ddbce
Ailken <ailkencf@gmail.com>
2023-08-06 14:31:12 -0300

    exercicios de métodos de String exercicio 04(formatação de dados do formulario)

--- 04/index.js
+++ 04/index.js
@@ -1,3 +1,23 @@
 let identificador = "123";
-let nome = "José silva costa";
-let email = "      jose@email.com  ";
+let nome = "Ailken xxxxx sxxxxx";
+let email = "      ailkenxx@email.com  ";
+
+function formatIdentificador(identificador) {
+    return identificador.padStart(6, '0');
+}
+function formatNome(nome) {
+        return nome.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+}
+function formatEmail(email) {
+    return email.trim();
+}
+
+let objetoFormatado = {
+    identificador: formatIdentificador (identificador),
+    nome: formatNome(nome),
+    email: formatEmail(email)
+};
+
+console.log(objetoFormatado.identificador);
+console.log(objetoFormatado.nome);
+console.log(objetoFormatado.email);
